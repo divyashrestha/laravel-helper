d2b6059faf2daa3a8ac2f48d2daa891a4b056b2d
Divya Shrestha <divyashrestha042@gmail.com>
2024-02-22 15:53:26 +0545

    fixed the test

---
+++ tests/Feature/InitialTest.php
@@ -0,0 +1,18 @@
+<?php
+namespace DivyaShrestha\LaravelHelper\Tests
+;
+
+use Orchestra\Testbench\TestCase;
+
+class InitialTest extends TestCase
+{
+
+    /**
+     * @test
+     */
+    public function initial_test(): void
+    {
+        $this-> assertTrue(true);
+    }
+    
+}
